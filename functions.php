18d6d200925bcc7f32a4c1955e56ecc073a1cdea
Gabor Javorszky <gabor@javorszky.co.uk>
2014-01-29 11:53:19 +0000

    Added metabox into the theme

--- functions.php
+++ functions.php
@@ -2,6 +2,12 @@
 
 $f = get_stylesheet_directory() . '/functions/';
 
+// Re-define meta box path and URL
+define( 'RWMB_URL', trailingslashit( get_stylesheet_directory_uri() . '/lib/meta-box' ) );
+define( 'RWMB_DIR', trailingslashit( get_stylesheet_directory() . '/lib/meta-box' ) );
+// Include the meta box script
+require_once RWMB_DIR . 'meta-box.php';
+
 include( $f . 'misc.php');
 include( $f . 'users.php');
 include( $f . 'ss.php');
