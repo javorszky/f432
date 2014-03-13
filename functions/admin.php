c7799059545090e53b72c9ccebba2aeeaae81d1b
Gabor Javorszky <gabor@javorszky.co.uk>
2014-03-13 11:10:46 +0000

    Changed login styling, minor refactor
    Made code more compact, removed unused files, flattened file system.

--- functions/admin.php
+++ functions/admin.php
@@ -9,31 +9,6 @@
 
 
 /**
- * Function to determine whether we're on register or login pages
- * @return [type] [description]
- */
-function es_is_login_page() {
-    return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
-}
-
-
-/**
- *
- * a function to add a custom style to the login form and other areas of admins
- * @return html
- * @author	Swifty <james@electricstudio.co.uk>
- * @since 	1.0
- */
-function enq_admin_stylesheet() {
-    if( ( is_admin() ) || es_is_login_page() ) {
-        wp_enqueue_style( 'admin-stylesheet', get_bloginfo('template_directory').'/functions/admin/es-admin-styles.css' );
-    }
-}
-add_action('admin_enqueue_scripts', 'enq_admin_stylesheet');
-add_action('login_enqueue_scripts', 'enq_admin_stylesheet');
-
-
-/**
  *
  * Adds ES logo and link to admin bar
  * @param 	object $wp_admin_bar
@@ -44,7 +19,7 @@ function add_es_logo( $wp_admin_bar ) {
     $wp_admin_bar->remove_node('wp-logo');
     $args = array(
         'id' => 'es_logo',
-        'title' => '<img src="'.get_bloginfo('template_directory') . '/functions/admin/images/es_icon.png"/>',
+        'title' => '<span class="ab-icon"><img src="'.get_bloginfo('template_directory') . '/functions/images/es_icon.png"/></span>',
         'href' => 'http://www.electricstudio.co.uk',
     );
     $wp_admin_bar->add_node( $args );
@@ -91,4 +66,16 @@ function wpc_dashboard_widgets() {
 // Add actions and filters for the functions in this file
 add_action('wp_dashboard_setup', 'wpc_dashboard_widgets');
 add_filter('admin_footer_text', 'remove_footer_admin');
-// add_filter('login_headerurl', 'wpc_url_login');
+add_action( 'admin_bar_menu', 'add_es_logo', 1 );
+
+function replace_logo() {
+    $url = get_template_directory_uri() . '/functions/images/es_logo_2014.png';
+    ?>
+    <style>
+        .login h1 a {
+            background-image: url(<?php echo $url; ?>);
+        }
+    </style>
+    <?php
+}
+add_action('login_head', 'replace_logo');
