6c092a6db8389ec35d7c6170e77e6ceaf556655a
Gabor Javorszky <gabor@javorszky.co.uk>
2014-06-30 14:47:31 +0100

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div class="row">
+	<div class="large-12 columns">
+		<h2>404: Not found</h2>
+		<p>The page you're looking for is not here.</p>
+	</div>
+</div>
+<?php get_footer();
