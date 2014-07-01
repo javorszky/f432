4428954e1e219ee97673d6b33a061795d85e2c03
Gabor Javorszky <gabor@javorszky.co.uk>
2014-07-01 15:55:43 +0100

    Added custom extraction

--- functions/misc.php
+++ functions/misc.php
@@ -22,4 +22,16 @@ function init_constants() {
 add_action( 'init', 'init_constants' );
 
 
+/**
+ * To avoid php notices for missing indexes
+ * @param  string  			$key     		the key of the meta
+ * @param  array 			$custom  		the entire custom object with get_post_custom
+ * @param  boolean 			$all     		whether to include ALL, or just one singular
+ * @param  string  			$default 		what the value should be returned in case there's no index like that
+ * @return mixed           					whatever the custom holds
+ */
+function es_custom( $key, $custom, $all = false, $default = '') {
+    return (array_key_exists($key, $custom)) ? ($all) ? $custom[$key] : $custom[$key][0] : $default;
+}
+
 
