35b06a5a66e21a11c9b7efbb022ff853066dc402
lukeames <luke@electricstudio.co.uk>
2014-07-21 12:58:38 +0100

    Commit before live

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 	<footer>
-
+		<div class="row">
+			<div class="large-6 columns">
+				&#169; Radius Equity LTD <?php echo date("Y"); ?>
+			</div>
+		</div>
 	</footer>
 	<?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -4,10 +4,15 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width" />
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 		<title><?php wp_title(); ?></title>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
 		<header>
-
+			<div class="row">
+				<div class="large-12 columns">
+					<?php wp_nav_menu( array( 'theme_location' => 'primary') ); ?>
+				</div>
+			</div>
 		</header>
