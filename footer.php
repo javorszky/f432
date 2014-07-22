a56a1b492fbb270c54f7176de192dfc063cb3374
Andrew Hudson <ahudson@gmail.com>
2014-07-22 15:01:34 +0000

    Revert "Commit before live": incorrectly added to the wrong repo by @lukeames
    This reverts commit 35b06a5a66e21a11c9b7efbb022ff853066dc402.

--- footer.php
+++ footer.php
@@ -1,9 +1,5 @@
 	<footer>
-		<div class="row">
-			<div class="large-6 columns">
-				&#169; Radius Equity LTD <?php echo date("Y"); ?>
-			</div>
-		</div>
+
 	</footer>
 	<?php wp_footer(); ?>
 </body>
